a0a0307909546b97761ed97b3adba3b11ea3766b
Adrien DE SEDE <adrien.de.sede@gmail.com>
2024-06-19 06:32:21 +0200

    feat: can decode lists

--- lib/bittorrent.ex
+++ lib/bittorrent.ex
@@ -2,8 +2,13 @@ defmodule Bittorrent.CLI do
   def main(argv) do
     case argv do
       ["decode" | [encoded_str | _]] ->
-        decoded_str = Bencode.decode(encoded_str)
-        IO.puts(Jason.encode!(decoded_str))
+        case Bencode.decode(encoded_str) do
+          {decoded_str, _remaining} ->
+            IO.puts(Jason.encode!(decoded_str))
+
+          ret ->
+            ret
+        end
 
       [command | _] ->
         IO.puts("Unknown command: #{command}")
@@ -17,29 +22,65 @@ defmodule Bittorrent.CLI do
 end
 
 defmodule Bencode do
+  defp decode_number(binary_data) do
+    case Enum.find_index(binary_data, fn char -> char == ?e end) do
+      nil ->
+        IO.puts("String starts with 'i' but does not have the 'e' suffix")
+
+      index ->
+        numberStr = List.to_string(Enum.slice(binary_data, 0..(index - 1)))
+        remaining = Enum.slice(binary_data, (index + 1)..-1//1)
+        {String.to_integer(numberStr), remaining}
+    end
+  end
+
+  defp decode_string(binary_data) do
+    case Enum.find_index(binary_data, fn char -> char == ?: end) do
+      nil ->
+        IO.puts("The ':' character is not found in the binary")
+
+      index ->
+        prefix = Enum.slice(binary_data, 0..(index - 1))
+        length = String.to_integer(List.to_string(prefix))
+        str = Enum.slice(binary_data, index + 1, length)
+        remaining = Enum.slice(binary_data, (index + 1 + length)..-1//1)
+        {List.to_string(str), remaining}
+    end
+  end
+
+  defp decode_list(binary_data, acc \\ []) do
+    case binary_data do
+      [] ->
+        {Enum.reverse(acc), []}
+
+      [?e | remaining] ->
+        {Enum.reverse(acc), remaining}
+
+      _ ->
+        {decoded, remaining} = decode(binary_data)
+        decode_list(remaining, [decoded | acc])
+    end
+  end
+
+  def decode(encoded_value) when is_list(encoded_value) do
+    decode(List.to_string(encoded_value))
+  end
+
   def decode(encoded_value) when is_binary(encoded_value) do
     binary_data = :binary.bin_to_list(encoded_value)
 
     case binary_data do
-      [?i | rest] ->
-        case Enum.find_index(rest, fn char -> char == ?e end) do
-          nil ->
-            IO.puts("String starts with 'i' but does not have the 'e' suffix")
+      [] ->
+        :empty
 
-          index ->
-            numberStr = List.to_string(Enum.slice(rest, 0..(index - 1)))
-            String.to_integer(numberStr)
-        end
+      [?l | tail] ->
+        decode_list(tail)
+
+      [?i | tail] ->
+        decode_number(tail)
 
       _ ->
-        case Enum.find_index(binary_data, fn char -> char == ?: end) do
-          nil ->
-            IO.puts("The ':' character is not found in the binary")
-
-          index ->
-            rest = Enum.slice(binary_data, (index + 1)..-1)
-            List.to_string(rest)
-        end
+        decode_string(binary_data)
     end
   end
 
