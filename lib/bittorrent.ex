aa904e68403506a58f0fa7d69bb09d3bd73d9983
Adrien DE SEDE <adrien.de.sede@gmail.com>
2024-06-21 08:04:48 +0200

    refactor: More idiomatic elixir
    Got help by looking at other people's solution

--- lib/bittorrent.ex
+++ lib/bittorrent.ex
@@ -1,167 +1,164 @@
 defmodule Bittorrent.CLI do
-  def main(argv) do
-    case argv do
-      ["decode" | [encoded_str | _]] ->
-        case Bencode.decode(encoded_str) do
-          :empty ->
-            IO.puts("Nothing to decode")
-
+  def main(["decode" | tail]) do
+    case tail do
+      [str] ->
+        case Bencode.decode(str) do
           {:error, reason} ->
             IO.puts(reason)
 
-          {decoded_str, _remaining} ->
-            IO.puts(Jason.encode!(decoded_str))
+          {decoded, remaining} ->
+            IO.puts(Jason.encode!(decoded))
+
+            if remaining != "" do
+              IO.puts("Warning ! Incomplete encoded string")
+            end
         end
 
-      # For testing purposes
-      ["encode" | _] ->
-        val = %{"foo" => "bar", "abc" => "def", "num" => 12, "list" => [13, 2, 3, 4, 5]}
-        IO.puts(Bencode.encode(val))
+      _ ->
+        IO.puts("Usage: your_bittorrent.sh decode <encoded_string>")
+    end
+  end
 
-      ["info" | [filename | _]] ->
-        file = File.read!(filename)
-        content = IO.iodata_to_binary(file)
+  def main(["encode"]) do
+    test_value = %{
+      "foo" => "bar",
+      "abc" => "def",
+      "n" => 42,
+      "list" => [1, 2, "three"],
+      "dict" => %{
+        "is_dict" => "yep"
+      }
+    }
+
+    IO.puts(Bencode.encode(test_value))
+  end
+
+  def main(["info" | tail]) do
+    case tail do
+      [filename] ->
+        content =
+          File.read!(filename)
+          |> IO.iodata_to_binary()
 
         case Bencode.decode(content) do
-          :empty ->
-            IO.puts("Nothing to decode")
-
           {:error, reason} ->
             IO.puts(reason)
 
-          {decoded_str, _remaining} ->
-            IO.puts("Tracker URL: #{decoded_str["announce"]}")
-            IO.puts("Length: #{decoded_str["info"]["length"]}")
-            # Bencode.encode(decoded_str)
+          {decoded_content, _} ->
+            hash =
+              :crypto.hash(:sha, Bencode.encode(decoded_content["info"]))
+              |> Base.encode16(case: :lower)
+
+            IO.puts("Tracker URL: #{decoded_content["announce"]}")
+            IO.puts("Length: #{decoded_content["info"]["length"]}")
+            IO.puts("Info Hash: #{hash}")
         end
 
-      [command | _] ->
-        IO.puts("Unknown command: #{command}")
-        System.halt(1)
-
-      [] ->
-        IO.puts("Usage: your_bittorrent.sh <command> <args>")
-        System.halt(1)
+      _ ->
+        IO.puts("Usage: your_bittorrent.sh info <path to torrent file>")
     end
+  end
+
+  def main([command | _]) do
+    IO.puts("Unknown command: #{command}")
+    System.halt(1)
+  end
+
+  def main([]) do
+    IO.puts("Usage: your_bittorrent.sh <command> <args>")
+    System.halt(1)
   end
 end
 
 defmodule Bencode do
-  defp decode_number(binary_data) do
-    case Enum.find_index(binary_data, fn char -> char == ?e end) do
-      nil ->
-        {:error, "`e` suffix not found"}
+  def decode(encoded_binary) when is_binary(encoded_binary),
+    do: do_decode(encoded_binary)
 
-      index ->
-        numberStr = List.to_string(Enum.slice(binary_data, 0..(index - 1)))
-        remaining = Enum.slice(binary_data, (index + 1)..-1//1)
-        {String.to_integer(numberStr), remaining}
+  def decode(_), do: {:error, "Unsupported type"}
+
+  defp do_decode(""), do: :empty
+  defp do_decode("l" <> encoded_list), do: decode_list(encoded_list, [])
+  defp do_decode("d" <> encoded_dict), do: decode_dict(encoded_dict, %{})
+
+  defp do_decode("i" <> encoded_number) do
+    case Integer.parse(encoded_number) do
+      {int, "e" <> remaining} -> {int, remaining}
+      {_, _} -> {:error, "Failed to parse integer, wrong format. Missing `e` suffix ?"}
+      :error -> {:error, "Failed to parse integer, invalid number"}
     end
   end
 
-  defp decode_string(binary_data) do
-    case Enum.find_index(binary_data, fn char -> char == ?: end) do
-      nil ->
-        {:error, "`:` delimiter not found"}
+  defp do_decode(encoded_binary), do: decode_binary(encoded_binary)
 
-      index ->
-        prefix = Enum.slice(binary_data, 0..(index - 1))
-        length = String.to_integer(List.to_string(prefix))
-        str = Enum.slice(binary_data, index + 1, length)
-        remaining = Enum.slice(binary_data, (index + 1 + length)..-1//1)
-        {List.to_string(str), remaining}
-    end
-  end
+  # Didn't write this function, took it from someone else on Codecrafters
+  # My initial version was much less idiomatic and I wanted to see how theirs works
+  # Value is not guaranteed to be UTF8
+  # <length represented as ASCII>:<value>
+  defp decode_binary(encoded_binary) do
+    case Integer.parse(encoded_binary) do
+      :error ->
+        {:error, "Failed to parse integer, invalid length"}
 
-  defp decode_list(binary_data, acc \\ []) do
-    case binary_data do
-      [] ->
-        {Enum.reverse(acc), []}
-
-      [?e | remaining] ->
-        {Enum.reverse(acc), remaining}
+      # If `String.length()` we count the number of graphemes, which doesn't
+      # work if value is not UTF8
+      {length, ":" <> encoded_value} when byte_size(encoded_value) >= length ->
+        <<decoded_value::binary-size(^length), remaining::binary>> = encoded_value
+        {decoded_value, remaining}
 
       _ ->
-        case decode(binary_data) do
+        {:error,
+         "Input does not match the `<length>:<value>` format or <value> is smaller than specified by <length>"}
+    end
+  end
+
+  defp decode_list("e" <> remaining, acc), do: {Enum.reverse(acc), remaining}
+
+  defp decode_list(encoded_list, acc) do
+    case decode(encoded_list) do
+      {:error, reason} ->
+        {:error, reason}
+
+      {decoded_value, remaining} ->
+        decode_list(remaining, [decoded_value | acc])
+    end
+  end
+
+  defp decode_dict("e" <> remaining, map), do: {map, remaining}
+
+  defp decode_dict(encoded_dict, map) do
+    case decode_binary(encoded_dict) do
+      {:error, _} ->
+        {:error, "Dict key must be a string"}
+
+      {key, remaining} ->
+        case decode(remaining) do
           {:error, reason} ->
             {:error, reason}
 
-          {decoded, remaining} ->
-            decode_list(remaining, [decoded | acc])
+          {value, remaining} ->
+            decode_dict(remaining, Map.merge(map, %{key => value}))
         end
     end
   end
 
-  defp decode_dict(binary_data, acc \\ %{}) do
-    case binary_data do
-      [] ->
-        {acc, []}
-
-      [?e | remaining] ->
-        {acc, remaining}
-
-      _ ->
-        case decode_string(binary_data) do
-          {:error, reason} ->
-            {:error, "Key could not be decoded: #{reason}"}
-
-          {key, remaining} ->
-            {value, remaining} = decode(remaining)
-            decode_dict(remaining, Map.put(acc, key, value))
-        end
-    end
-  end
-
-  def decode(data) when is_list(data) do
-    case data do
-      [] ->
-        :empty
-
-      [?l | tail] ->
-        decode_list(tail)
-
-      [?i | tail] ->
-        decode_number(tail)
-
-      [?d | tail] ->
-        decode_dict(tail)
-
-      _ ->
-        decode_string(data)
-    end
-  end
-
-  def decode(encoded_value) when is_binary(encoded_value) do
-    decode(:binary.bin_to_list(encoded_value))
-  end
-
-  def decode(_), do: "Invalid encoded value: not binary"
-
-  def encode(string) when is_binary(string), do: "#{String.length(string)}:#{string}"
-
+  def encode(binary) when is_binary(binary), do: "#{byte_size(binary)}:" <> binary
   def encode(number) when is_number(number), do: "i#{number}e"
 
   def encode(list) when is_list(list) do
-    {_, encodedList} =
-      Enum.map_reduce(list, "", fn item, byteArray ->
-        encodedItem = encode(item)
-        {encodedItem, "#{byteArray}#{encodedItem}"}
-      end)
+    items =
+      Enum.map(list, fn item -> encode(item) end)
+      |> Enum.join()
 
-    "l#{encodedList}e"
+    "l" <> items <> "e"
   end
 
   def encode(map) when is_map(map) do
-    {_, encodedMap} =
-      Enum.map_reduce(Map.keys(map), "", fn key, byteArray ->
-        encodedKey = encode(key)
-        encodedVal = encode(map[key])
+    items =
+      Map.keys(map)
+      |> Enum.sort()
+      |> Enum.map(fn key -> encode(key) <> encode(Map.get(map, key)) end)
+      |> Enum.join()
 
-        {{encodedKey, encodedVal}, "#{byteArray}#{encodedKey}#{encodedVal}"}
-      end)
-
-    "d#{encodedMap}e"
+    "d" <> items <> "e"
   end
-
-  def encode(_), do: "Unsupported data type"
 end
